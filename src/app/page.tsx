02b671d9ae0509045cd8904a1d582af1a1efa2b0
Vishesh_techie <visheshhirisave@gmail.com>
2024-02-25 20:32:18 +0530

    commited

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
+import HeroSection from "./components/HeroSection";
+import FeaturedCourses from "./components/FeaturedCourses";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
     
     <h1 className="text-2xl text-center text-white">Vishesh Musics</h1>
-    
+    <HeroSection/>
+    <FeaturedCourses/>
     </main>
   
   );
